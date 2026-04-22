71cdf959d8fb8f9a69be2e5d4a10ec399f9f7b07
Alexander Schaefer <135496460+mcwkrsk@users.noreply.github.com>
2026-04-22 16:51:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Lab2_Task11;
+using Lab2_Task12;
+using Lab2_Task2_Async;
+using Lab2_Task2_Sync;
+using System;
+using System.Text;
+
+namespace Lab2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Task2Sync.Run();
+             // Task2Async.Run().GetAwaiter().GetResult();
+        }
+    }
+}
